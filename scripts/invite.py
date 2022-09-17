c06e2673156622daabbb66a9b4381c0fec5f1463
jasmaa <jasonmaa3955@gmail.com>
2022-09-16 22:23:20 -0400

    Add pinning

--- scripts/invite.py
+++
@@ -1,8 +0,0 @@
-import os
-import discord
-from dotenv import load_dotenv
-
-load_dotenv()
-
-if __name__ == '__main__':
-    print(f'{discord.utils.oauth_url(os.getenv("DISCORD_APPLICATION_ID"), discord.Permissions(8208))}')
